07eac8f212eb74e483892c3c6748df75a7ff821e
Malte I.Lauterbach <malte@enigma-blog.de>
2020-11-10 23:03:57 +0100

    fixed js (hopefully, im fixing in production!)

--- js/blog.js
+++ js/blog.js
@@ -31,28 +31,28 @@ function commandProcessor(e){
   if(e.keyCode == 13){
 
     //Clear the area where info will be printed
-    document.getElementById('injected').innerHTML= "";
+    document.getElementById('injected_blog').innerHTML= "";
 
     //Get user input
     var txtInput = document.getElementById('txtBox').value;
 
     //Select what info to print according to command
     if(txtInput == "help"){
-      document.getElementById('injected').innerHTML=help();
+      document.getElementById('injected_blog').innerHTML=help();
     }else if (txtInput=="all") {
-      document.getElementById('injected').innerHTML=quick() + "\n\n\n" + skills() + "\n\n\n" + legal() + "\n\n\n" + contact() + "\n\n\n";
+      document.getElementById('injected_blog').innerHTML=quick() + "\n\n\n" + skills() + "\n\n\n" + legal() + "\n\n\n" + contact() + "\n\n\n";
     }else if (txtInput == "quick") {
-      document.getElementById('injected').innerHTML=quick();
+      document.getElementById('injected_blog').innerHTML=quick();
     }else if (txtInput == "contact") {
-      document.getElementById('injected').innerHTML=contact();
+      document.getElementById('injected_blog').innerHTML=contact();
     }else if (txtInput=="skills") {
-      document.getElementById('injected').innerHTML=skills();
+      document.getElementById('injected_blog').innerHTML=skills();
     }else if (txtInput=="legal") {
-      document.getElementById('injected').innerHTML=legal();
+      document.getElementById('injected_blog').innerHTML=legal();
     }else if (txtInput == "scratchpad") {
-      document.getElementById('injected').innerHTML=scratchpad();
+      document.getElementById('injected_blog').innerHTML=scratchpad();
     }else{
-      document.getElementById('injected').innerHTML = help();
+      document.getElementById('injected_blog').innerHTML = help();
     }
 
     //Clear input text box
